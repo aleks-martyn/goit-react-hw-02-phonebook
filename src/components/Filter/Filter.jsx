04e50b773fbb0d2b99880cdc0b499f45480710c5
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-05 01:32:06 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
 
-export const Filter = () => {
+export const Filter = ({ value, onChange }) => {
   return (
     <label>
       <span>Find contacts by name</span>
-      <input type='text'/>
+      <input type="text" value={value} onChange={onChange} />
     </label>
   );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
