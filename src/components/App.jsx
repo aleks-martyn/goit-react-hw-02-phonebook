8db024c8a19ad1149773845017ffbe16eea0a160
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-04 22:22:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import shortid from 'shortid';
 import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList';
 import { Container } from './App.styled';
 
 export class App extends Component {
@@ -11,16 +12,18 @@ export class App extends Component {
 
   formSubmitHandler = data => {
     const contact = { id: shortid.generate(), ...data };
-    
+
     this.setState(prevState => ({
       contacts: [contact, ...prevState.contacts],
     }));
   };
 
   render() {
+    const { contacts } = this.state;
     return (
       <Container>
         <ContactForm onSubmit={this.formSubmitHandler} />
+        <ContactList users={contacts} />
       </Container>
     );
   }
