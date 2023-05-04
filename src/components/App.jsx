7ff8092bb70213704023176e2dd868b936a96577
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-04 22:44:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,12 @@ import { Container } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -22,7 +27,9 @@ export class App extends Component {
     const { contacts } = this.state;
     return (
       <Container>
+        <h1>Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
         <ContactList users={contacts} />
       </Container>
     );
