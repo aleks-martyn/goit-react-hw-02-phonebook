9692bb8d7129d25f2b518cde625241317ef184fe
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-05 00:44:53 +0300

    Update Filter.jsx

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+
+export const Filter = () => {
+  return (
+    <label>
+      <span>Find contacts by name</span>
+      <input type='text'/>
+    </label>
+  );
+};
