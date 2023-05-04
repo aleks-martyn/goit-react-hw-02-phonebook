1c7f96c6e19d77aa0dfe5414215210c62edb0c42
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-04 22:56:10 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -27,6 +27,7 @@ export class ContactForm extends Component {
   };
 
   render() {
+    const { name, number } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
         <label htmlFor={this.nameInputId}>
@@ -38,7 +39,7 @@ export class ContactForm extends Component {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          value={this.state.name}
+          value={name}
           onChange={this.handleNameChange}
           id={this.nameInputId}
         />
@@ -52,7 +53,7 @@ export class ContactForm extends Component {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          value={this.state.number}
+          value={number}
           onChange={this.handleNameChange}
           id={this.numberInputId}
         />
