a67a43311c961f389dc1e68ce8120fa4a0ca38ce
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-04 18:02:30 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,11 +3,12 @@ import { Component } from 'react';
 export class ContactForm extends Component {
   state = {
     name: '',
+    number: '',
   };
 
   handleNameChange = event => {
     console.log(event.currentTarget.value);
-    this.setState({ name: event.currentTarget.value });
+    this.setState({ [event.currentTarget.name]: event.currentTarget.value });
   };
 
   handleSubmit = event => {
@@ -27,10 +28,27 @@ export class ContactForm extends Component {
           <input
             type="text"
             name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
             value={this.state.name}
             onChange={this.handleNameChange}
           />
         </label>
+
+        <label>
+          <span>Number</span>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleNameChange}
+          />
+        </label>
+
         <button type="submit">Add contact</button>
       </form>
     );
