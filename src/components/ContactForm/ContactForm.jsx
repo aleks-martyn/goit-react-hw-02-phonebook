9b0c4efbc9cc33b89d6153ce9c158837f8557535
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-04 15:06:38 +0300

    Update ContactForm.jsx

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+class ContactForm extends Component {
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const form = evt.currentTarget;
+    const login = form.elements.login.value;
+    const password = form.elements.password.value;
+    console.log(login, password);
+    this.props.onSubmit({ login, password });
+    form.reset();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="login" />
+        <input type="password" name="password" />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
