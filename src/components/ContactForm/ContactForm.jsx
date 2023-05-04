9233587b311e256966004f36b6a44202071a7af2
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-04 21:36:42 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import shortid from 'shortid';
 
 export class ContactForm extends Component {
@@ -61,3 +62,7 @@ export class ContactForm extends Component {
     );
   }
 }
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
