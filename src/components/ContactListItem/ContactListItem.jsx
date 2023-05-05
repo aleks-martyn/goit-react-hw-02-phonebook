6b3e25932a4e2ae61e99826db335b60de2f04596
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-05 18:47:38 +0300

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
 
-export const ContactListItem = ({ userName, userNumber }) => (
+export const ContactListItem = ({
+  userId,
+  userName,
+  userNumber,
+  onDeleteContact,
+}) => (
   <li>
     <p>
       {userName}: <span>{userNumber}</span>
     </p>
-    <button type="button">Delete</button>
+    <button type="button" onClick={() => onDeleteContact(userId)}>
+      Delete
+    </button>
   </li>
 );
 
 ContactListItem.propTypes = {
+  userId: PropTypes.string.isRequired,
   userName: PropTypes.string.isRequired,
   userNumber: PropTypes.string.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
 };
