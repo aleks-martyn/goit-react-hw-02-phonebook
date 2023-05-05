edccfc6f52178c8411c3a059fc9fc615feb41ace
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-05 23:22:13 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 
-export const Filter = ({ value, onChange }) => {
-  return (
-    <label>
-      <span>Find contacts by name</span>
-      <input type="text" value={value} onChange={onChange} />
-    </label>
-  );
-};
+export const Filter = ({ value, onChange }) => (
+  <label>
+    <span>Find contacts by name</span>
+    <input type="text" value={value} onChange={onChange} />
+  </label>
+);
 
 Filter.propTypes = {
   value: PropTypes.string.isRequired,
